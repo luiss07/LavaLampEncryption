651e71a9056aed1f538ba7144725db5fa61d330b
luiss07 <luigi.delleva@gmail.com>
2022-12-12 11:41:41 +0100

    added library for msp

--- MSP432/uartMspEsp/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Universita/3anno/IoT/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
-
