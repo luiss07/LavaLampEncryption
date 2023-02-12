783bfa4998cbbb94ea7ab08ed60f09f0cf2c50ea
LucaCavalcanti <101122897+LucaCavalcanti@users.noreply.github.com>
2023-02-12 16:29:00 +0100

    Add includes for visualization and LCD usage

--- MSP432/main.c
+++ MSP432/main.c
@@ -1,3 +1,10 @@
+#include <ti/devices/msp432p4xx/inc/msp.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/grlib/grlib.h>
+#include "LcdDriver/Crystalfontz128x128_ST7735.h"
+#include "LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h"
+#include <stdio.h>
+#include <math.h>
 #include "msp.h"
 
 
