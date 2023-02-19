5d9c12ed281bba697ed80b5a3d3fef8391eaa4b7
LucaCavalcanti <101122897+LucaCavalcanti@users.noreply.github.com>
2023-02-19 15:25:51 +0100

    Modularized interface section and added User Pushbuttons usage

---
+++ MSP432/libs/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIGS
+#define CONFIGS
+
+#define ROWS_FOR_HASH_NUMBER 5
+#define DIGITS_OF_HASHED_NUMBER 97
+#define DIGITS_OF_HASHED_NUMBER_ARRAY 4
+#define MAX_DIGITS_INLINE 19
+#define MAX_MENU_SELECTIONS 4
+#define MAX_RANDOM_NUMBER_DIGITS 10
+#define GENERATE_PAGE_SELECTIONS 5
+#define SETTINGS_PAGE_SELECTIONS 3
+#define MAX_THEMES 3
+
+// Define the pins for the Booster Pack buttons
+#define BP1_PORT GPIO_PORT_P5
+#define BP1_PIN GPIO_PIN1
+#define BP2_PORT GPIO_PORT_P3
+#define BP2_PIN GPIO_PIN5
+
+#endif
