e51e3f49e7f5502d0aff15da02ce85a4ef1bb5f0
Odin <rgermenia@gmail.com>
2023-02-21 15:51:57 +0100

    typo

--- ESP32-Wrover-Cam/src/main.cpp
+++ ESP32-Wrover-Cam/src/main.cpp
@@ -72,7 +72,7 @@ void setup()
   WiFi.disconnect(); // Disconnect from WiFi network, if WiFi was not disconnected properly last time
   delay(1000); // wait for WiFi to disconnect
 
-  WiFi.begin(WIFI_SSID, PASSWORD_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   if (SERIAL_DEBUG) { Serial.print("Connecting to Wifi"); }
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -362,7 +362,7 @@ void loop()
     WiFi.disconnect(); // reset wifi
     delay(1000);
 
-    WiFi.begin(WIFI_SSID, PASSWORD_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     // wait for the wifi to connect
     if (SERIAL_DEBUG) { Serial.print("Connecting to WiFi"); }
     while( WiFi.status() != WL_CONNECTED){
